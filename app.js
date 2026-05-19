e079a6f53965c269c01f81f1cf1d1a0bd2b7f216
JonathanVw10 <jojojonathan790@gmail.com>
2026-05-19 22:23:46 +0700

    5 Initial Base Dashboard

--- app.js
+++ app.js
@@ -41,6 +41,18 @@ app.get('/status', (req, res) => {
   res.json({ status: 'OK', message: 'ShipShape API is running' });
 });
 
+app.get('/admin', (req, res) => {
+  res.render('pages/admin_home');
+});
+
+app.get('/staf-admin', (req, res) => {
+  res.render('pages/staf_admin_home');
+});
+
+app.get('/kaprodi', (req, res) => {
+  res.render('pages/kaprodi_home');
+});
+
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Not Found' });
 });
